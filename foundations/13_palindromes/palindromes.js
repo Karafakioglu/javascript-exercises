2e95f2cd2ea3c29a200ef7e8886c722a77d5ec37
Ismail Karafakioglu <ismailkarafakioglu@gmail.com>
2026-08-07 00:07:42 +0200

    finish 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,57 @@
-const palindromes = function () {
+// const palindromes = function (string) {
+//     let lowerCaseString = string.toLowerCase()
+//     let splitArr = lowerCaseString.split("");
+//     let reversedArr = []
+//     let arrMinus = splitArr.length - 1
+    
+//     for (let i = 0; i < splitArr.length; i++) {
+//         reversedArr[i] = splitArr[arrMinus - i]
+        
+//     }
+
+//     let splitArrString = splitArr.join("")
+//     let reversedArrString = reversedArr.join("")
+
+//     if (splitArrString === reversedArrString){
+//         return true
+//     }else{
+//         return false
+//     }
+
+// };
+
+const palindromes = function (string) {
+    let punctuationClean = string.replace(/\W/g, "")
+    let lowerCaseString = punctuationClean.toLowerCase()
+    let splitArr = lowerCaseString.split("");
+    let reversedArr = []
+    let arrMinus = splitArr.length - 1
+    
+    for (let i = 0; i < splitArr.length; i++) {
+        reversedArr[i] = splitArr[arrMinus - i]
+        
+    }
+
+    let splitArrString = splitArr.join("")
+    let reversedArrString = reversedArr.join("")
+
+    if (splitArrString === reversedArrString){
+        return true
+    }else{
+        return false
+    }
 
 };
+
+
+
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
