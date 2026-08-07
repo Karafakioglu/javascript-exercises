64d3ee669037e7526d3a6b2ec1366b938e0f52d0
Ismail Karafakioglu <ismailkarafakioglu@gmail.com>
2026-08-07 22:56:21 +0200

    complete getTheTitles exercise

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    return arr.map((bookObj) =>{
+        return bookObj.title
+    })
 };
 
 // Do not edit below this line
