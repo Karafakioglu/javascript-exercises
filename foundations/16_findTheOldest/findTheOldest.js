80288896fc8e9df9ed8acd16043ceefdf46a95d6
Ismail Karafakioglu <ismailkarafakioglu@gmail.com>
2026-08-08 01:13:11 +0200

    solve findTheOldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArr) {
 
+    let peopleAlive = peopleArr.map((person) =>{
+        if(!person.yearOfDeath){
+            return {...person, yearOfDeath: new Date().getFullYear()}
+        }else{
+            return {...person}
+        }
+    })
+    console.log(peopleAlive)
+    
+    let oldestPeopleInOrder =  peopleAlive.sort((a,b) =>{
+        let totalYearA = a.yearOfDeath - a.yearOfBirth
+        let totalYearB = b.yearOfDeath - b.yearOfBirth
+        return totalYearB - totalYearA
+    })
+    return oldestPeopleInOrder[0]
 };
 
 // Do not edit below this line
