2105639ac8a0467e350c61ef6e0368ea11ae3e6f
Ismail Karafakioglu <ismailkarafakioglu@gmail.com>
2026-08-07 22:39:31 +0200

    solve fibonnaci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num)
 
+    if(num < 0)
+        return "OOPS"
+    if(num === 0)
+        return 0
+    else if(num === 1){
+        return 1
+    }else{
+        let first = 0
+        let second = 1
+        let third
+
+        for(let i = 0; i < num - 1; i++){
+            third = first + second
+            first = second
+            second = third
+        }
+        return third
+    }
 };
 
 // Do not edit below this line
